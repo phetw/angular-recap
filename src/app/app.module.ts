425873f6d5f270304b2253c0bc02f57759d0d2fb
was.li <wasuwat.se@gmail.com>
2018-07-26 12:36:07 +0700

    [✅] add finishing touch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,14 @@ import { RouterModule, Routes } from '@angular/router'
 import { NgModule } from '@angular/core'
 import { FormsModule } from '@angular/forms'
 import { HttpClientModule } from '@angular/common/http'
+import { SampleModule } from './sample/sample.module'
 
 import { AppComponent } from './app.component'
 import { NavbarComponent } from './navbar/navbar.component'
 import { HomeComponent } from './home/home.component'
 import { AboutComponent } from './about/about.component'
 
-const appRoutes: Routes = [
+const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   {
@@ -21,15 +22,7 @@ const appRoutes: Routes = [
 
 @NgModule({
   declarations: [AppComponent, NavbarComponent, AboutComponent, HomeComponent],
-  imports: [
-    BrowserModule,
-    HttpClientModule,
-    FormsModule,
-    RouterModule.forRoot(
-      appRoutes,
-      { enableTracing: true } // <-- debugging purposes only
-    )
-  ],
+  imports: [BrowserModule, HttpClientModule, FormsModule, SampleModule, RouterModule.forRoot(routes)],
   providers: [],
   bootstrap: [AppComponent]
 })
